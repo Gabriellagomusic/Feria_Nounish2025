481ca7724dbc10c7e11bb43e21c39daaac0335e3
Sweets Sweetman <sweetmantech@gmail.com>
2025-08-22 15:59:21 -0500

    remove metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,34 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { GeistSans } from "geist/font/sans"
-import { GeistMono } from "geist/font/mono"
-import { MiniKitContextProvider } from "@/providers/MiniKitProvider"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { MiniKitContextProvider } from "@/providers/MiniKitProvider";
+import "./globals.css";
 
 export async function generateMetadata(): Promise<Metadata> {
-  const URL = "https://ferianounish2025.vercel.app"
+  const URL = "https://ferianounish2025.vercel.app";
   return {
     title: "Feria Nounish 2025",
     description: "Feria Nousnish",
     openGraph: {
-      title:"Feria Nounish 2025",
+      title: "Feria Nounish 2025",
       description: "Feria Nousnish",
       images: "https://ferianounish2025.vercel.app/images/feria-logo.png",
     },
     other: {
-      'fc:frame': JSON.stringify({
-        version: 'next',
+      "fc:frame": JSON.stringify({
+        version: "next",
         imageUrl: "https://ferianounish2025.vercel.app/images/feria-logo.png",
         button: {
           title: "Entrar a Feria Nounish 2025",
           action: {
-            type: 'launch_frame',
+            type: "launch_frame",
             name: "Feria Nounish 2025",
             url: URL,
-            splashImageUrl: "https://ferianounish2025.vercel.app/images/feria-logo.png",
-            splashBackgroundColor: process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
+            splashImageUrl:
+              "https://ferianounish2025.vercel.app/images/feria-logo.png",
+            splashBackgroundColor:
+              process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
           },
         },
       }),
@@ -37,7 +39,7 @@ export async function generateMetadata(): Promise<Metadata> {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -54,9 +56,5 @@ html {
         <MiniKitContextProvider>{children}</MiniKitContextProvider>
       </body>
     </html>
-  )
+  );
 }
-
-export const metadata = {
-      generator: 'v0.app'
-    };
